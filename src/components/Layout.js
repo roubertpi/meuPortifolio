237a1d078716105666c41312c62df2882546ef1e
roubertpi <roubertpimentar@gmail.com>
2021-02-28 23:08:34 -0300

    Meu primeiro commit

---
+++ src/components/Layout.js
@@ -0,0 +1,14 @@
+import React from "react"
+// import "../css/main.css"
+import Navbar from "./Navbar"
+import Sidebar from "./Sidebar"
+import Footer from "./Footer"
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navbar/>
+       </>
+  )
+}
+
+export default Layout
